7acd0dc91961a39767fa6675dbd3446e9acad8ba
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-03 09:42:26 +0530

    Add user input handling for guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,8 +7,20 @@ fn main() {
 
     println!("Please input your guess:");
 
-    let guess: String = String::new();
+    let mut guess: String = String::new();
 
-    io::stdin().read_line(guess);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
     
+    println!("You guessed {}", guess);
 }
+
+/* So there are two types of Creates
+    - Binary Crates 
+    - Library Crates
+
+    Binary crates are executable on its own, whereas library crates are not, its intended to be used in other programs.
+
+    rand is a library crates to generate random number.
+*/
