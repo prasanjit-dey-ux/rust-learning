88415ca4d6cfdaa5fdcb6ccf0b0e323584d1017a
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-03 08:31:45 +0530

    added varibles in rust

---
+++ variable/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // using mut to make it mutable
+    let mut age = 22;
+
+    age = 23;
+    
+    println!("The current age is {age}");
+}
+
+/*  So in rust the variables are immutable 
+    - Rust assumes safty first
+    - if value dont change, it reduce bugs
+
+    To make it mutable we use mut
+    eg:
+
+    let mut x = 6;
+    x = 7;
+
+    So In Rust, nothing changes unless you explicitly allow it
+*/
