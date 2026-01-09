81ab62e83e19a5f238ffb183be9ef134d2c7a122
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-09 21:28:52 +0530

    added data_types in rust

---
+++ data_types/scaler_type/src/main.rs
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+fn main() {
+    let int_num = 10;
+    let float_num = 3.16;
+    let is_active = true;
+    let letter = "P";
+
+    println!("{int_num}, {float_num}, {is_active}, {letter}");
+}
