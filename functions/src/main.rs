2df9159472394919f90464aa5176da0987232cd1
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-14 00:33:53 +0530

    learned about early return

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -8,11 +8,20 @@ fn main() {
     // if we add ; then its a statement if not then a expression, lets see
 
     let y = {
-        let x = 4;
-        x + 1
+        let x = 4; // statement
+        x + 1 // exoression
     };
+        println!("The value of y is: {y}");
+        
+    let p = substraction(10, 5); // in rust we dont use named argument when calling function.
+    println!("value of p is {p}");
 
-    println!("The value of y is: {y}");
+    let q = another_example(5);
+    println!("The value of q is {q}");
+
+    let check = is_even(4);
+    println!("The check is a {check}");
+
 }
 
 fn another_function(x: i32) { // this is the parameter
@@ -29,3 +38,23 @@ fn addition(x: i32, y: i32) {
 }
 
 /* Calling a function is an expression */
+
+
+fn substraction(x: i32, y: i32) -> i32 {
+    x - y
+}
+
+fn another_example(x:i32) -> i32 {
+    x + 1
+}
+
+/* Early return */
+
+fn is_even(x: i32) -> bool {
+    
+    if x % 2 == 0 {
+        return true;
+    }
+    println!("the number is not even");
+    return false; 
+}
